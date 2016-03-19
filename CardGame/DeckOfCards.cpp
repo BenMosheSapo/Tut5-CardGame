540c5e32b78e9ef056449e4345ab425b51c2c6b1
Ben Sapo <bensapo@gmail.com>
2016-03-20 00:17:11 +0200

    added reset, draw and peek functions

--- CardGame/DeckOfCards.cpp
+++ CardGame/DeckOfCards.cpp
@@ -17,35 +17,54 @@ DeckOfCards::DeckOfCards()
 	}
 }
 
+Card DeckOfCards::draw() {
+	if (numCards != 0) {
+		Card temp = *(Deck[numCards-1]);
+		delete Deck[numCards-1];
+		numCards -= 1;
+		return temp;
+	}
+	else
+		cout << "Sorry there are no more cards in the Deck"<<endl;
+}
+
+int DeckOfCards::numberOfCards() {
+	return numCards;
+}
+
+Card DeckOfCards::peek() {
+
+	return *Deck[numCards-1];
+}
+
 DeckOfCards DeckOfCards::reset(){
 	return DeckOfCards();
 }
 
 void DeckOfCards::shuffle(){
-	int i;
-	Card* temp;
-	int random,random2;
-	for (i = 0; i < 50; i++){
-		random = rand() % numCards;
-		random2 = rand() % numCards;
-		temp = Deck[random];
-		Deck[random] = Deck[random2];
-		Deck[random2] = temp;
+	if (numCards != 0) {
+		int i;
+		Card* temp;
+		int random, random2;
+		for (i = 0; i < 50; i++) {
+			random = rand() % numCards;
+			random2 = rand() % numCards;
+			temp = Deck[random];
+			Deck[random] = Deck[random2];
+			Deck[random2] = temp;
+		}
 	}
+	else
+		cout << "Sorry there are no more cards in the Deck"<<endl;
 }
 
 void DeckOfCards:: print() {
 	int i;
-	for (i = 0; i < 20; i++) {
+	for (i = 0; i < numCards; i++) {
 		cout << (*(Deck[i])).Number() << (*(Deck[i])).Colour()<< endl;
 	}
 }
-//Card DeckOfCards::draw(){
-//	Card temp = Deck[0];
-//	delete Deck;
-//
-//
-//}
+
 DeckOfCards::~DeckOfCards()
 {
 }
@@ -53,6 +72,10 @@ DeckOfCards::~DeckOfCards()
 int main(){
 	DeckOfCards c;
 	c.shuffle();
-	c.print();
+	Card Temp;
+	int i;
+	for (i = 0; i < 25; i++) {
+		Temp = c.draw();
+	}
 	return 0;
 }
